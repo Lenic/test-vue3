ac67cb569b0dd73ef69869d494e0a79bcb4f6afd
Lenic <Lenic@live.cn>
2020-10-31 18:06:12 +0800

    feat(*): 修复 Compose 返回值类型定义异常
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/utils/compose.ts
+++ src/utils/compose.ts
@@ -45,13 +45,13 @@ const ComposeFunc = <R, T>(
 export const compose = <R, T>(
   defaultAction: (options: T) => ComposeResult<R>,
   ...plugins: ComposePlugin<R, T>[]
-): ComposeDirection<R, T> =>
+): ComposeInstance<R, T> =>
   ComposeFunc(ComposeDirection.LEFT_TO_RIGHT, defaultAction, plugins);
 
 export const composeRight = <R, T>(
   defaultAction: (options: T) => ComposeResult<R>,
   ...plugins: ComposePlugin<R, T>[]
-): ComposeDirection<R, T> =>
+): ComposeInstance<R, T> =>
   ComposeFunc(ComposeDirection.RIGHT_TO_LEFT, defaultAction, plugins);
 
 export default ComposeFunc;
