294f938e2f3d1975647ca8de213a0d40a5376f8e
Lenic <Lenic@live.cn>
2020-10-31 14:53:58 +0800

    feat(*): 更新 Compose 执行逻辑
    - 添加带有执行方向的参数以及缺省执行逻辑
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/utils/compose.ts
+++ src/utils/compose.ts
@@ -1,32 +1,49 @@
 export type ComposeResult<T> = T | Promise<T>;
 
+export enum ComposeDirection {
+  LEFT_TO_RIGHT = true,
+  RIGHT_TO_LEFT = false
+}
+
 export interface ComposePlugin<R, T> {
   (next: () => ComposeResult<R>, arg: T): ComposeResult<R>;
 }
 
 export interface ComposeInstance<R, T> {
-  add(newPlugins: ComposePlugin<R, T>[]): ComposeInstance<R, T>;
+  add(...newPlugins: ComposePlugin<R, T>[]): ComposeInstance<R, T>;
   exec(options: T): ComposeResult<R>;
 }
 
-export interface ComposeType<R, T> {
-  (plugins: ComposePlugin<R, T>[]): ComposeInstance<R, T>;
-}
-
-const Compose = <R, T>(
+const ComposeFunc = <R, T>(
+  direction: ComposeDirection,
+  defaultAction: (options: T) => ComposeResult<R>,
   plugins: ComposePlugin<R, T>[]
 ): ComposeInstance<R, T> => ({
-  add(newPlugins) {
-    return Compose([...plugins, ...newPlugins]);
+  add(...newPlugins) {
+    return ComposeFunc(direction, defaultAction, [...plugins, ...newPlugins]);
   },
   exec(options) {
-    const func = plugins.reduce(
+    const method =
+      direction === ComposeDirection.LEFT_TO_RIGHT
+        ? plugins.reduceRight
+        : plugins.reduce;
+    const func = method.call(
+      plugins,
       (acc, x) => () => x(acc, options),
-      () => plugins[0](null, options)
-    );
+      () => defaultAction(options)
+    ) as () => ComposeResult<R>;
 
     return func();
   }
 });
 
-export default Compose;
+export const compose: ComposeInstance<R, T> = <R, T>(
+  defaultAction: (options: T) => ComposeResult<R>,
+  ...plugins: ComposePlugin<R, T>[]
+) => ComposeFunc<R, T>(ComposeDirection.LEFT_TO_RIGHT, defaultAction, plugins);
+export const composeRight: ComposeInstance<R, T> = <R, T>(
+  defaultAction: (options: T) => ComposeResult<R>,
+  ...plugins: ComposePlugin<R, T>[]
+) => ComposeFunc(ComposeDirection.RIGHT_TO_LEFT, defaultAction, plugins);
+
+export default ComposeFunc;
