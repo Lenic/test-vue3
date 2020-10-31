f6a37fb0a2b070722149c4f274186e5f1c09ce69
Lenic <Lenic@live.cn>
2020-10-31 14:56:21 +0800

    feat(*): 添加 Compse 方法的使用示例
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,43 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "/@/components/HelloWorld.vue"; // @ is an alias to /src
+import { composeRight } from "../utils/compose";
 
 export default defineComponent({
   name: "Home",
   components: {
     HelloWorld
+  },
+  setup() {
+    let dd = composeRight<number, number>((c) => {
+      debugger;
+      console.log("default");
+
+      return c + 1;
+    });
+
+    dd = dd.add((n, c) => {
+      debugger;
+      console.log("run 2:");
+
+      return c + 2 + n();
+    });
+
+    dd = dd.add((n, c) => {
+      debugger;
+      console.log("run 3:");
+
+      return c + 3 + n();
+    });
+
+    dd = dd.add((n, c) => {
+      debugger;
+      console.log("run 4:");
+
+      return c + 4 + (n ? n() : 0);
+    });
+
+    console.log("result", dd.exec(10));
   }
 });
 </script>
